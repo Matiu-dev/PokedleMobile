0a1b24ba9ab90576ad450304312f8b1e1aa1a859
Matiu-dev <matiw97@wp.pl>
2025-07-19 10:59:32 +0200

    connection with server

--- app/src/main/java/pl/matiu/pokebdemobile/data/api/PokeApi.kt
+++ app/src/main/java/pl/matiu/pokebdemobile/data/api/PokeApi.kt
@@ -1,9 +1,14 @@
 package pl.matiu.pokebdemobile.data.api
 
+import android.util.Log
 import io.ktor.client.*
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
+import io.ktor.http.URLProtocol
+import io.ktor.http.append
+import io.ktor.http.path
+import pl.matiu.pokebdemobile.domain.entity.PokemonModel
 
 class PokeApi {
     suspend fun getPokemonSpeciesDataAsString(pokemonName: String): String {
@@ -12,5 +17,22 @@ class PokeApi {
 
     suspend fun getPokemonDataAsString(pokemonName: String): String {
         return HttpClient(CIO).get("https://pokeapi.co/api/v2/pokemon/${pokemonName}").body()
+    }
+
+    suspend fun getPokemonModel(pokemonName: String): String {
+        return try {
+            HttpClient(CIO).get {
+                url {
+                    host = "matiu-dev.site"
+
+                    protocol = URLProtocol.HTTPS
+                    path("saveAllPokemonsToDatabase")
+                    parameters.append("pokemonName", pokemonName)
+                }
+            }.body()
+        } catch (e: Exception) {
+            Log.d("HTTP", "Błąd: ${e.message}", e)
+            "ERROR: ${e.message}"
+        }
     }
 }
